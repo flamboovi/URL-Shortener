5fb9994fe9d1fbdf8c54fb54c7a2925eb70a427f
flamboovi <mdowais325@gmail.com>
2023-07-08 16:02:58 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NgTinyUrlService } from 'ng-tiny-url';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'URL_Shortener';
+  model={
+    inputUrl:''
+  }
+  isFormSubmitted=false;
+  isLoading=false;
+  shortUrl='';
+  isTextCopied=false;
+  constructor(private tinyUrl:NgTinyUrlService){}
+  onSubmitUrlForm(urlForm:any){
+    if(urlForm.valid){
+      this.isLoading=true;
+      this.tinyUrl.shorten(this.model.inputUrl).subscribe((data)=>{
+        this.shortUrl=data;
+        this.isFormSubmitted=true;
+        this.isLoading=false;
+      },(error)=>{
+        alert("Somethiing went wrong, Please check your URL and try again");
+        this.isLoading=false;
+      })
+    }
+  }
+
+  //reset
+
+  reset(){
+    this.model.inputUrl='';
+    this.isFormSubmitted=false;
+    this.isTextCopied=false;
+  }
+
+  //copy
+
+  copyUrl(shortUrlElementRef:any){
+    let inputElement=document.createElement('input');
+    inputElement.setAttribute('type','text');
+    inputElement.setAttribute('value',shortUrlElementRef.innerHTML);
+    inputElement.select();
+    inputElement.setSelectionRange(0,999999);  //for Mobile Selection
+    try{
+      navigator.clipboard.writeText(inputElement.value);
+      this.isTextCopied=true;
+      setTimeout(()=>{
+        this.isTextCopied=false;
+      },2000);
+    }catch(e:any){
+      console.warn("Error while copying...",e.toString());
+    }
+
+  }
 }
